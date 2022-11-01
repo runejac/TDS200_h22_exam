71764afb2064e329ae91ad6dee2f08c503a8e1ae
runejac <69790920+runejac@users.noreply.github.com>
2022-11-01 17:14:31 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TDS200_h22_1012',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
